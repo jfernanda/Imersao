800ad0c94ff5b6a5fa008cfb0df0a638b06abd93
J. Fernanda <santosl.jfer@gmail.com>
2024-09-04 21:45:17 +0100

    Alterações da Aula 03, inclusão do arquivo dados.js

---
+++ dados.js
@@ -0,0 +1,89 @@
+let dados = [
+    {
+        nome : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma das maiores promessas do skate mundial. Ela já conquistou diversos títulos e se tornou um ícone para a nova geração de skatistas. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        nome : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é um ícone do esporte brasileiro, especialmente da ginástica artística. Com um talento inegável e uma determinação admirável, ela conquistou o coração dos brasileiros e se tornou a maior medalhista olímpica da história do país. Suas conquistas, como o ouro no salto e a prata no individual geral nos Jogos Olímpicos de Tóquio, inspiraram uma nova geração de atletas e consolidaram seu lugar na história do esporte.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        nome : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos. Com sua técnica apurada e determinação, ela conquistou a medalha e se tornou um exemplo para jovens atletas. Sua trajetória inspiradora mostra a força e a perseverança do esporte brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
+
+let filmesGhibli = [
+    {
+      titulo: "A Viagem de Chihiro",
+      sinopse: "Chihiro, uma menina de 10 anos, se encontra em um mundo espiritual após seus pais serem transformados em porcos. Ela deve trabalhar em uma casa de banho para humanos para ser libertada e ajudar seus pais.",
+      genero: "Fantasia",
+      duracao: 125
+    },
+    {
+      titulo: "Meu Vizinho Totoro",
+      sinopse: "Duas irmãs se mudam para o campo com o pai e descobrem um mundo mágico habitado por criaturas encantadoras, como o Totoro.",
+      genero: "Fantasia",
+      duracao: 87
+    },
+    {
+        titulo: "O Castelo Animado",
+        sinopse: "Sophie, uma jovem moça, é transformada em uma velha por um bruxo e deve quebrar o feitiço com a ajuda de um príncipe que se transformou em um pássaro.",
+        genero: "Fantasia",
+        duracao: 134
+      },
+      {
+        titulo: "A Princesa Mononoke",
+        sinopse: "Em um Japão medieval, uma jovem princesa luta para proteger a floresta de humanos que buscam seus recursos.",
+        genero: "Aventura, Fantasia",
+        duracao: 134
+      },
+      {
+        titulo: "Kiki Entregas a Domicílio",
+        sinopse: "Kiki, uma jovem bruxa, deve encontrar um lugar para viver e trabalhar enquanto aprimora seus poderes mágicos.",
+        genero: "Fantasia",
+        duracao: 102
+      }
+  ];
+
+  let pokemons = [
+    {
+      nome: "Pikachu",
+      tipo: "Elétrico",
+      geracao: 1,
+      origem: "Kanto"
+    },
+    {
+      nome: "Bulbasaur",
+      tipo: "Grama, Veneno",
+      geracao: 1,
+      origem: "Kanto"
+    },
+    {
+      nome: "Charmander",
+      tipo: "Fogo",
+      geracao: 1,
+      origem: "Kanto"
+    },
+    {
+      nome: "Squirtle",
+      tipo: "Água",
+      geracao: 1,
+      origem: "Kanto"
+    },
+    {
+      nome: "Chikorita",
+      tipo: "Grama",
+      geracao: 2,
+      origem: "Johto"
+    },
+    {
+      nome: "Cyndaquil",
+      tipo: "Fogo",
+      geracao: 2,
+      origem: "Johto"
+    }
+  ];
